f7734b258f52c314aab9bfa051722e8a31351758
Ashlay M Ambrose <ashlayamb@gmail.com>
2025-05-20 15:15:48 +0530

    Added basic player movements

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,10 +9,24 @@ public class PlayerMovement : MonoBehaviour
     }
 
     public Rigidbody Rigidbody;
+    public float ForwardForce=1000f;
+    public float SideForce = 500f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Rigidbody.AddForce(0,0,1000*Time.deltaTime);
+        Rigidbody.AddForce(0,0,ForwardForce*Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            Rigidbody.AddForce(SideForce * Time.deltaTime, 0, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            Rigidbody.AddForce(-SideForce * Time.deltaTime, 0, 0, 0);
+        }
+
+
+
     }
 }
